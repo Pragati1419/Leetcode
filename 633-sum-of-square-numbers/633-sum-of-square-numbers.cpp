c1bef8cd65f2e646c55b1b50b6f81c7c903012be
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-02-12 11:29:20 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.57%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        
+      long long int a=0;
+        long long int b=sqrt(c);
+    while(a<=b){
+        if(a*a+b*b==c){
+            return true;
+        }
+        else if(a*a+b*b<c){
+            a++;
+        }
+        else{
+            b--;
+        }
+    }
+    return false;
+        
+    
+
+    }
+};
