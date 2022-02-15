d129b5c403e81d0e54d2d0c74be969ecd0f04737
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-02-15 13:25:01 +0530

    Time: 16 ms (91.50%), Space: 17 MB (19.26%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int val = nums[0];
+        for(int i=1;i<nums.size();i++){
+            val = val^nums[i];
+        }
+        return val;
+    }
+
+    
+    
+};
