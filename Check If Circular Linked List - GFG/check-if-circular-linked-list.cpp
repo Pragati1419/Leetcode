ac283e3a25111b6f5bfe206fab1a9d3987b5136c
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-03-21 20:28:58 +0530

    Added solution - LeetHub

--- Check If Circular Linked List - GFG/check-if-circular-linked-list.cpp
+++ Check If Circular Linked List - GFG/check-if-circular-linked-list.cpp
@@ -76,15 +76,14 @@ bool isCircular(Node *head)
    if (head == NULL)
        return true;
  
-    // Next of head
+
     struct Node *node = head->next;
  
-    // This loop would stop in both cases (1) If
-    // Circular (2) Not circular
+
     while (node != NULL && node != head)
        node = node->next;
  
-    // If loop stopped because of circular
-    // condition
+
     return (node == head);
+
 }
