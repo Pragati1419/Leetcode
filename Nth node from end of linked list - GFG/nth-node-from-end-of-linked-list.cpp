c158bbc3451e8808342fc17b58e34cc162f81d1f
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-04-02 11:01:22 +0530

    Added solution - LeetHub

--- Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.cpp
+++ Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.cpp
@@ -63,34 +63,38 @@ int main()
 int getNthFromLast(Node *head, int n)
 {
        // Your code here
-       Node *curr = head;
-    	Node *prev = NULL;
-    	
-    	int total = 0;
-    	
-    	//Reverse linked list
-    	while(curr != NULL) {
-    	    total++;
-    	    Node *temp = curr->next;
-    	    curr->next = prev;
-    	    prev = curr;
-    	    curr = temp;
-    	}
-    	
-    	if(total<n) {
-    	    return -1;
-    	}
-    	
-    	int count = 0;
-    	while(prev != NULL) {
-    	    count++;
-    	    if(count == n) {
-    	        return prev->data;
-    	    }
-    	    prev = prev->next;
-    	}
-    	
-    	return head->data;
+           Node *currNode = head , *nthNode = head;
+    
+    //traversing first n elements with first pointer.
+    while(n!=0)
+        {
+            if(currNode==NULL)
+                return -1;
+            currNode = currNode->next;
+            n--;
+        }
+    //now traversing with both pointers and when first pointer gives null 
+    //we have got the nth node from end in second pointer since the first 
+    //pointer had already traversed n nodes and thus had difference of n 
+    //nodes with second pointer.
+    while(currNode!=NULL)   
+        {
+            nthNode = nthNode->next;
+            currNode = currNode->next;
+        }
+    
+    //returning the data of nth node from end.
+    if(nthNode!=NULL)
+        return nthNode->data;
+    else
+        return -1;
+        
+
+
+
+
     
 }
+       
+       
 
