53b5ef1117b60ccdd6eb88efa9df3fdc6bbec57e
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-04-04 08:07:40 +0530

    Added solution - LeetHub

--- Move all zeros to the front of the linked list - GFG/move-all-zeros-to-the-front-of-the-linked-list.cpp
+++ Move all zeros to the front of the linked list - GFG/move-all-zeros-to-the-front-of-the-linked-list.cpp
@@ -72,16 +72,30 @@ struct Node
 void moveZeroes(struct Node **head)
 {
     //Your code here
-        Node *cur=*head;
-    while(cur && cur->next){
-        if(cur->next->data==0){
-            Node* k=cur->next;
-            Node* n=k->next;
-            k->next=*head;
-            *head=k;
-            cur->next=n;
-        }
-        else
-            cur=cur->next;
-}
+         struct Node *temp = (*head)->next, *prev = *head;
+  while (temp != NULL) {
+ 
+    // If current node is 0, move to
+    // beginning of linked list
+    if (temp->data == 0) {
+ 
+      // Disconnect node from its
+      // current position
+      Node *curr = temp;
+      temp = temp->next;
+      prev->next = temp;
+ 
+      // Move to beginning
+      curr->next = (*head);
+      *head = curr;
+    }
+ 
+    // For non-zero values
+    else {
+      prev = temp;
+      temp = temp->next;
+    }
+  }
+
+
 }
