d43819d9dc4529a679b3cfb7cfd72fb82d96dbad
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-06-26 21:34:42 +0530

    Added solution - LeetHub

--- Missing number in array - GFG/missing-number-in-array.cpp
+++ Missing number in array - GFG/missing-number-in-array.cpp
@@ -14,17 +14,17 @@ class Solution{
   public:
     int MissingNumber(vector<int>& array, int n) {
         // Your code goes here
-        sort(array.begin(), array.end());
-        for(int i=1; i<n; i++)
-        {
-            if(i!=array[i-1])
-            {
-                return i;
-            }
-        }
         
-    }
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            sum+=array[i];
             
+        }
+        int result=0;
+        result=(n*(n+1)/2)-sum;
+        return result;
+    
+    }
 };
 
 // { Driver Code Starts.
