c6fe5e552fd24ea2650d13002bcf41b6eaaba963
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-07-03 18:12:08 +0530

    Time: 24 ms (91.40%), Space: 31 MB (87.55%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-          
-         int leftsum=0;
-        int rightsum=0;
-
-         for(int number: nums) // Adding the rightsum
-           rightsum = rightsum + number;
-/* Traversing through whole array to find total sum and set the rightsum equals to itself
-minus our current value at the position we are at. */
-
-              for(int i=0;i<nums.size();i++)
-              {  
-                  rightsum = rightsum-nums[i];
-                     if(leftsum==rightsum)
-                     {
-                         return i;  
-                     }
-                    
-                     leftsum = leftsum + nums[i];
-              
-              }
+        int n = nums.size();
+      if(n== 0)
+          return -1;
         
-        return -1;
-
+       
+        int totalsum = 0, leftsum = 0;
+        
+        for(int i : nums)
+            totalsum += i;
+        
+        for(int i = 0; i < n; i++) {
+            if(leftsum == (totalsum - nums[i]))
+                return i;
+            else {
+                leftsum += nums[i];
+                totalsum -= nums[i];
+                
+            }
+        }
+        
+        return -1;  
     }
 };
