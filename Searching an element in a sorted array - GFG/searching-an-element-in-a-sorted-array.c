a7a5f85bdc5c6d035338d23f10d44626c2dcef4a
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-07-03 19:46:33 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.c
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.c
@@ -11,12 +11,22 @@ int searchInSorted(int arr[], int N, int K)
 { 
     
        // Your code here
-       for(int i=0;i<N;i++){
-           if(arr[i]==K)
-           return 1;
-           
+       int low=0;
+       int high=N-1;
+       
+       while(low<=high){
+           int mid=low+(high-low)/2;
+           if(arr[mid]==K){
+               return 1;
+           }
+               else if(arr[mid]>K){
+                   high=mid-1;
+               }
+               else{
+                   low=mid+1;
+               }
        }
-       return -1;
+           return -1;
        
 }
 
