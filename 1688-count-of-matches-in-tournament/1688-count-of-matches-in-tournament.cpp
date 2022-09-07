f5b42a67b5414e13e07ebea60fd43a568d673326
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-09-07 17:40:34 +0530

    Time: 3 ms (37.45%), Space: 6 MB (25.05%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n-1;
+       // return n-1;
+        //burte force approach
+         int count=0;
+      while(n>1){
+        
+          count =count+(n/2);
+          
+        n=1+(n-1)/2;
+        }
+      
+      return count;
     }
 };
