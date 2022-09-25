f513dd12aba583ae6db5290487a114fa82132ae1
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-09-25 13:41:36 +0530

    Time: 38 ms (5.68%), Space: 6 MB (39.42%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n < 2) 
-            return n;
-    	int a = 0, b = 1, c = 0;
-        for(int i = 1; i < n; i++)
-        {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return c;
+      if(n<2)
+          return n;
+        return fib(n-1)+fib(n-2);
     }
 };
