620b1ddcc1aca0bfc7949fae15fd564b869d1ee1
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-10-12 22:34:04 +0530

    Time: 31 ms (17.47%), Space: 12.8 MB (27.13%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+       int n = nums.size();
+     /*   priority_queue<pair<int,int>> pq;
+       int ans=0;
+        unordered_map<int,int> m;
+        for(int i =0;i<n-1;i++){
+            if(nums[i]==key){
+                m[nums[i+1]]++;
+            }
+        
+        
+        for(auto i : m){
+            pq.push({i.second,i.first});
+        }
+         ans= pq.top().second;
+        }
+        return ans;
+        */
+         unordered_map<int,int> mp;
+        for(int i=0;i<n-1;++i)
+        {
+            if(nums[i]==key)
+                mp[nums[i+1]]++;
+        }
+        int ans=0,max=INT_MIN;
+        for(auto i : mp)
+        {
+            if(max<i.second)
+            {
+                max=i.second;
+                ans=i.first;
+            }
+        }
+        return ans;
+    }
+};
