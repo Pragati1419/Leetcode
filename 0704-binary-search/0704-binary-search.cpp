2e91d3fd4c42434b65c92fe8c9a6ab8cb4bb54d6
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2022-12-05 18:25:20 +0530

    Time: 129 ms (8.21%), Space: 27.6 MB (12.74%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-      for(int i=0;i<nums.size();i++){
-          if(nums[i]==target)
-              return i;
-         
-      }
+      int start = 0,end = nums.size() - 1;
+        
+		
+        while(start <= end)
+        {
+			//  mid
+            int mid = start + (end - start)/2;
+			
+			// if  mid is equals to target
+            if(nums[mid] == target)
+                return mid;
+				
+			// value at mid is less than target
+			// update the start by mid + 1
+			// key should be in range mid + 1 to end
+            else if(nums[mid] < target)
+                start = mid+1;
+				
+			// else update the end by mid - 1
+			// value should be in range 0 to mid - 1
+            else
+                end = mid-1;
+        }
+        
+		// if target doesn;t found
         return -1;
         
     }
+    
 };
