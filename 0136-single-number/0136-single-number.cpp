52316c20efda8fa75fc88b5624f418d288191336
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-04-21 22:18:59 +0530

    Time: 20 ms (63.60%), Space: 17 MB (56.64%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     int n = nums.size();
+        int XOR = 0;
+        for(int i=0;i<n;i++){
+            XOR^=nums[i];
+        }
+        return XOR;
+    }
+};
