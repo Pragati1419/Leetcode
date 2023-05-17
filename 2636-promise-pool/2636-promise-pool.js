a05b96aeaa073377838d1bc866d5e8a47ee77595
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-05-17 12:08:01 +0530

    Time: 70 ms (21.34%), Space: 41.1 MB (100.00%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+ return new Promise((resolve) => {
+        let inProgress = 0, index = 0;
+        function helper() {
+            // base case
+            if (index >= functions.length) {
+                if (inProgress === 0)
+                    resolve();
+                return;
+            }
+
+            while (inProgress < n && index < functions.length) {
+                inProgress++;
+                functions[index++]().then(() => {
+                    inProgress--;
+                    helper();
+                })
+            }
+        }
+        helper();
+    })
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
