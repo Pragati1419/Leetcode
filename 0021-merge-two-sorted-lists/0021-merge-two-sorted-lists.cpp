6243b2ffe065a7bea6723e70f98c6653910d4d60
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-06-14 11:04:16 +0530

    Time: 8 ms (70.73%), Space: 14.8 MB (26.70%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,15 +11,23 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //Burute force Approach 
+        if (!list1 || list2 && list1->val > list2->val) 
+           swap(list1, list2);
+    if (list1)
+        list1->next = mergeTwoLists(list1->next, list2);
+    return list1;  
+      /*
+      Without using extra space
+      if(list2==NULL)
+            return list1;
         if(list1==NULL)
             return list2;
-        if(list2==NULL)
-            return list1;
         if(list1->val>list2->val)
             swap(list1,list2);
         ListNode *res=list1;
         while(list1!=NULL && list2!=NULL){
-            ListNode *temp=NULL;
+            ListNode *temp = NULL;
             while(list1!=NULL && list1->val<=list2->val){
                 temp=list1;
                 list1=list1->next;
@@ -28,6 +36,6 @@ public:
             swap(list1,list2);
         }
         return res;
-            
+        */
     }
 };
